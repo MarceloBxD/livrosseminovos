8ae1e35de575a1ebeb52871baf3bfdd3d1134646
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-06-07 19:37:22 -0300

    feat: adding api and hiding filter and sort on products page

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -36,7 +36,7 @@ export default function Navbar() {
       </div>
       <div className="center">
         <Link className="link" to="/">
-          Biblioteca Particular
+          Recicle Livros
         </Link>
       </div>
       <div className="right">
