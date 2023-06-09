868f79f272fba1d7f35ca527a72f34896b70feae
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-06-09 20:56:37 -0300

    feat: changing photos logic

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,20 +1,27 @@
 import { Link } from "react-router-dom";
 import "./styles.scss";
-import React from "react";
+import React, { useState } from "react";
 import { useApp } from "../../contexts/ContextApi";
 import { useFetch } from "../../hooks/useFetch";
 
 export default function Card({ item }) {
   const { products } = useFetch("/produtos?populate=*");
-  // const {} = useApp();
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("");
+
+  var firstImage =
+    "http://localhost:1337" + item.attributes.imagem1.data.attributes.url;
+  var secondImage =
+    "http://localhost:1337" + item.attributes.imagem2.data.attributes.url;
+  // "http://localhost:1337" + item.attributes.imagem1.data.attributes.url;
 
   return (
-    <Link className="link" to={`/product/${item?.id}`}>
+    <Link className="link" to={`/products/${item?.id}`}>
       <div className="card">
         <div className="image">
           {item?.attributes?.novo && <span>Novo</span>}
-          <img src={""} alt="" className="mainImg" />
-          <img src={""} alt="" className="secondImg" />
+          <img src={firstImage} alt="" className="mainImg" />
+          <img src={secondImage} alt="" className="secondImg" />
         </div>
         <h2>{item?.attributes?.titulo}</h2>
         <div className="prices">
--- client/src/contexts/ContextApi.jsx
+++ client/src/contexts/ContextApi.jsx
@@ -13,18 +13,12 @@ export function AppProvider({ children }) {
   const [products, setProducts] = useState([]);
   const [cartQuantity, setCartQuantity] = useState(0);
   const [open, setOpen] = useState(false);
-  const [image1, setImage1] = useState("");
-  const [image2, setImage2] = useState("");
 
   const value = {
     cartQuantity,
     setCartQuantity,
     open,
     setOpen,
-    image1,
-    setImage1,
-    image2,
-    setImage2,
     products,
     setProducts,
   };
