121bdadc9fdb26853009ac5226feb42eb9685df6
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-06-18 22:50:01 -0300

    feat: changing button text

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -26,7 +26,7 @@ export default function Home() {
       <Support />
       <TextImage
         title="Reviva a criança que há em você"
-        btnText={"Reviva a criança que há em você!"}
+        btnText={"Voltar aos produtos"}
         desc="A leitura é uma forma de viajar, de conhecer novos mundos e de se conectar com a criança que há em você. Abuse da imaginação e se divirta, afinal, a vida é muito curta para não se divertir, seja criança, seja feliz!"
         img="https://images.unsplash.com/photo-1590952203438-445a8699a380?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNyaWFuJUMzJUE3YSUyMGxlbmRvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
       />
